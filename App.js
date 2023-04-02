15e033f4f3651b4ddf4baa7822d7941e2bcf75a7
Amr Mohamed <arsmohamed@outlook.com>
2023-04-02 08:22:56 -0230

    done with the Exercise: Set up the navigation and there is the splash screen that will be worked on later on

--- App.js
+++ App.js
@@ -1,22 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react, {useState, useEffect} from 'react';
+import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Onboarding from './screens/Onboarding';
+import Profile from './screens/Profile';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchIsOnboardingCompleted() {
+      const value = await AsyncStorage.getItem('isOnboardingCompleted');
+      setIsOnboardingCompleted(value === 'true');
+      setIsLoading(false);
+    }
+    fetchIsOnboardingCompleted();
+  }, []);
+
+  if (isLoading) {
+     // We haven't finished reading from AsyncStorage yet
+    // return <SplashScreen />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Onboarding/>
-      {/* <Text>Testing</Text> */}
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <NavigationContainer  >
+      <Stack.Navigator>
+        {
+          isOnboardingCompleted ? 
+            (<Stack.Screen name="Profile" component={Profile} />
+            ): (<Stack.Screen name="Onboarding" component={Onboarding} /> )
+        }
+        {/* <Stack.Screen name="Onboarding" component={Onboarding} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
